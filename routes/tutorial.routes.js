b72ff42fa5947f94a740346a17b4f87fc7703e7f
https://github.com/github-NandiNotha <nandieh99@gmail.com>
2023-04-14 11:46:56 -0700

    done

--- routes/tutorial.routes.js
+++ routes/tutorial.routes.js
@@ -1,28 +1,36 @@
 module.exports = app => {
-    const tutorials = require("../app/controllers/tutorial.controller");
+
+  const tutorials = require("../controllers/tutorial.controller");
+
+  var router = require("express").Router();
+
+  //Create a new Tutorial
+  router.post("/", tutorials.create);
+
   
-    var router = require("express").Router();
-  
-    // Create a new Tutorial
-    router.post("/", tutorials.create);
-  
-    // Retrieve all Tutorials
-    router.get("/", tutorials.findAll);
-  
-    // Retrieve all published Tutorials
-    router.get("/published", tutorials.findAllPublished);
-  
-    // Retrieve a single Tutorial with id
-    router.get("/:id", tutorials.findOne);
-  
-    // Update a Tutorial with id
-    router.put("/:id", tutorials.update);
-  
-    // Delete a Tutorial with id
-    router.delete("/:id", tutorials.delete);
-  
-    // Delete all Tutorials
-    router.delete("/", tutorials.deleteAll);
-  // 
-    app.use('/api/tutorials', router);
-  };
+
+  //Retrieve all Tutorials
+  router.get("/", tutorials.findAll);
+
+  //Retrieve a single Tutorial with id
+  router.get("/:id", tutorials.findOne);
+
+  //Update a Tutorial with id
+  router.put("/:id", tutorials.update);
+
+  // //Delete a Tutorial with id
+  router.delete("/:id", tutorials.delete);
+
+  //Patch a Tutorial with id
+  router.patch("/:id", tutorials.update);
+
+  //Delete all Tutorials
+  router.delete("/", tutorials.deleteAll);
+
+  //Retrieve all published Tutorials
+  router.get("/published", tutorials.findAllPublished);
+
+  app.use("/api/tutorials", router);
+
+
+};
